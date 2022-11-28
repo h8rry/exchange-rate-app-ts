0a660252e7bd440070c6364a6a2e0947ded6e3e9
h8rry <git@harrychen.net>
2022-11-28 15:54:58 +0800

    RatesTable OK

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import './App.css';
 import getRates from './utils/getRates';
 import Navbar from './components/Navbar';
+import type { Rates } from './utils/getRates';
+import RatesTable from './components/RatesTable';
 import React from 'react';
 
 type AppProps = {};
 
-type AppStates = {};
+type AppStates = {
+  baseCurrency: string;
+  baseValue: number;
+  rates: Rates
+};
 
 class App extends React.Component<AppProps, AppStates> {
   constructor(props: any) {
     super(props);
 
-    this.state = {}
+    this.state = {
+      baseCurrency: 'USD',
+      baseValue: 1,
+      rates: [],
+    }
   }
 
   refreshRates = async() => {
     let rates = await getRates('USD');
     
-    console.log("Testing refreshRates");
-    console.log(rates);
+    if (rates) {
+      this.setState({ rates });
+    }
   }
 
   componentDidMount() {
@@ -29,6 +40,14 @@ class App extends React.Component<AppProps, AppStates> {
     return (
       <div className="App">
         <Navbar />
+
+        <main className="mx-auto max-w-xl px-4 sm:px-6 lg:px-8" hidden={this.state.rates.length === 0}>
+          <RatesTable
+            baseCurrency={this.state.baseCurrency}
+            baseValue={this.state.baseValue}
+            rates={this.state.rates}
+          />
+        </main>
       </div>
     );
   }
