b1261c4518b8a1a6726b7eb873ebc332b6528dcb
h8rry <git@harrychen.net>
2022-11-28 16:14:26 +0800

    SelectBaseCurrency OK

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import getRates from './utils/getRates';
 import Navbar from './components/Navbar';
 import type { Rates } from './utils/getRates';
 import RatesTable from './components/RatesTable';
+import SelectBaseCurrency from './components/SelectBaseCurrency';
 import React from 'react';
 
 type AppProps = {};
@@ -24,11 +25,32 @@ class App extends React.Component<AppProps, AppStates> {
     }
   }
 
+  changeBaseCurrency = async(baseCurrency: string) => {
+    this.setState({ 
+      baseCurrency,
+      rates: []
+    }, this.refreshRates)
+  }
+
+  changeBaseValue = (baseValue: number) => {
+    this.setState({
+      baseValue: Number(baseValue),
+      rates: []
+    }, this.refreshRates)
+  }
+
   refreshRates = async() => {
-    let rates = await getRates('USD');
+    let rates = await getRates(this.state.baseCurrency);
     
     if (rates) {
-      this.setState({ rates });
+      rates = rates.map((rate) => {
+        return {
+          ...rate,
+          total: rate.rate * this.state.baseValue
+        }
+      });
+
+      this.setState({ rates })
     }
   }
 
@@ -40,6 +62,25 @@ class App extends React.Component<AppProps, AppStates> {
     return (
       <div className="App">
         <Navbar />
+
+        <main className="mx-auto max-w-lg px-4 pt-10 pb-12 lg:pb-16">
+          <form>
+            <div className="space-y-6">
+              <div>
+                <h1 className="text-lg font-medium leading-6 text-gray-900">Currency Exchange Rates</h1>
+                <p className="mt-1 text-sm text-gray-500">
+                  Let’s get started by selecting a base currency.
+                </p>
+              </div>
+
+              <SelectBaseCurrency 
+                baseValue={this.state.baseValue}
+                changeBaseCurrency={this.changeBaseCurrency}
+                changeBaseValue={this.changeBaseValue}
+              />
+            </div>
+          </form>
+        </main>
 
         <main className="mx-auto max-w-xl px-4 sm:px-6 lg:px-8" hidden={this.state.rates.length === 0}>
           <RatesTable
